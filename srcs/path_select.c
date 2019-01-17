8a0c677a079f1629ad9235bbb0fc4594369a08f1
Sarunas VASKELIS <svaskeli@student.42.fr>
2019-01-17 11:07:18 +0100

    successfully removing dublicate paths

--- srcs/path_select.c
+++ srcs/path_select.c
@@ -6,13 +6,13 @@
 /*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 18:30:43 by svaskeli          #+#    #+#             */
-/*   Updated: 2019/01/16 19:33:55 by svaskeli         ###   ########.fr       */
+/*   Updated: 2019/01/17 11:06:08 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int		ft_compare(t_path *path, t_path *moving)
+int		ft_compare_path(t_path *path, t_path *moving)
 {
 	t_path_room	*highway;
 	t_path_room	*moving_high;
@@ -32,24 +32,23 @@ int		ft_compare(t_path *path, t_path *moving)
 		return (0);
 }
 
-void	ft_remove_path(t_lem_in *lem_in, t_path *path)
+void	ft_remove_path(t_lem_in *lem_in, t_path **path)
 {
-	t_path		*to_remove;
+	t_path		*remove;
 	t_path_room	*tmp;
 
-	to_remove = lem_in->paths;
-	while (to_remove && to_remove->next != path)
-		to_remove = to_remove->next;
-	to_remove->next = to_remove->next->next;
-	to_remove = to_remove->next;
-	while (to_remove && to_remove->highway)
+	remove = lem_in->paths;
+	while (remove && remove->next != *path)
+		remove = remove->next;
+	remove->next = remove->next->next;
+	while (*path && (*path)->highway)
 	{
-		tmp = to_remove->highway;
-		to_remove->highway = to_remove->highway->next;
-		free(tmp);
+		tmp = (*path)->highway;
+		(*path)->highway = (*path)->highway->next;
+ 		free(tmp);
 	}
-//	free(to_remove);
-//	to_remove = NULL;
+	free(*path);
+	*path = NULL;
 }
 
 void	ft_remove_dublicates(t_lem_in *lem_in)
@@ -63,13 +62,16 @@ void	ft_remove_dublicates(t_lem_in *lem_in)
 		moving_path = path->next;
 		while (moving_path)
 		{
-			if (ft_compare(path, moving_path))
+			if (ft_compare_path(path, moving_path))
 			{
-				ft_remove_path(lem_in, moving_path);
+				ft_remove_path(lem_in, &moving_path);
 				moving_path = path->next;
 			}
-			moving_path = moving_path->next;
+			if (moving_path)
+				moving_path = moving_path->next;
 		}
 		path = path->next;
 	}
 }
+
+
