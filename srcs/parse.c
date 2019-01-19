afa18de7f0c9f694ae28271e72366b47552db959
Drew FINNIS <dfinnis@e2r8p1.42.fr>
2019-01-19 15:59:15 +0100

    now accepts negative coordinates

--- srcs/parse.c
+++ srcs/parse.c
@@ -22,7 +22,7 @@ void	ft_parse_ants(t_lem_in *lem_in)
 			ft_lem_in_error(/*lem_in*/"invalid first line: number of ants");
 	if (!(lem_in->ant_count = ft_atoi(lem_in->line)))//correct atoi??
 		ft_lem_in_error(/*lem_in*/"invalid first line: number of ants");
-	if (lem_in->ant_count < 1)
+	if (lem_in->ant_count < 1)// or over MAX_ANTS
 		ft_lem_in_error(/*lem_in*/"number of ants out of acceptable range");
 }
 
@@ -49,10 +49,14 @@ void	ft_parse_room(t_lem_in *lem_in)
 		ft_lem_in_error(/*lem_in*/"y coordinate not digit");
 	while (lem_in->line[n] && ft_isdigit(lem_in->line[n]))
 		n--;
+	if (lem_in->line[n] == '-')
+		n--;
 	new->y = ft_atoi(lem_in->line + n + 1);
 	if (!ft_isdigit(lem_in->line[--n]))
 		ft_lem_in_error(/*lem_in*/"x coordinate not digit");
 	while (lem_in->line[n] && ft_isdigit(lem_in->line[n]))
+		n--;
+	if (lem_in->line[n] == '-')
 		n--;
 	new->x = ft_atoi(lem_in->line + n + 1);
 	if (!(new->name = ft_strndup(lem_in->line, n)))
