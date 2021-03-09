a0b02122a517cdcc683b6356742f2b04770310fe
Drew Finnis <drewfinnis@gmail.com>
2021-03-09 21:43:58 +0100

    feature/WIP refacto test best worst

--- test_performance.sh
+++ test_performance.sh
@@ -10,15 +10,24 @@ unit_test()
 	case=25
 	count=0
 	sum=0
-	while [ $count -lt $(expr $case + 1) ]
+	best=4242
+	worst=-4242
+	while [ $count -lt $case ]
 	do
-		echo "\r-- $count/$case --\c"
-		arg1=$(./generator $MAP > 1 && cat 1 | grep "#Here" | cut -d ' ' -f 8 | head -n 1)
-		arg2=$(./lem-in -t < 1 | tail -n 2 | head -n 1 | cut -d ' ' -f 2)
-		diff=$(($arg2 - $arg1))
-		# echo $diff ##########################
+		target=$(./generator $MAP > 1 && cat 1 | grep "#Here" | cut -d ' ' -f 8 | head -n 1)
+		real=$(./lem-in -t < 1 | tail -n 2 | head -n 1 | cut -d ' ' -f 2)
+		diff=$(($real - $target))
+		if [ $diff -lt $best ]
+		then
+			best=$diff
+		fi
+		if [ $diff -gt $worst ]
+		then
+			worst=$diff
+		fi
 		sum=$(($sum + $diff))
 		count=$(($count + 1))
+		echo "\r\x1B[32mPassed: $count/$count\x1B[0m\c"
 	done
 	count=$(($count - 1))
 	echo
@@ -26,17 +35,29 @@ unit_test()
 	# echo $sum
 	# echo $count
 	mean=$(($sum / $count))
-	# echo "\x1B[32mavg moves diff: $mean\x1B[0m"
 
+	echo "turns taken - required:"
+	if [ $best -le "0" ]
+	then
+		echo "\x1B[32mbest:  $best\x1B[0m"
+	else
+		echo "\x1B[33mbest:  $best\x1B[0m"
+	fi
 	if [ $mean -le "0" ]
 	then
-		echo "\x1B[32mavg moves diff: $mean\x1B[0m"
+		echo "\x1B[32mmean:  $mean\x1B[0m"
 	else
-		echo "\x1B[33mavg moves diff: $mean\x1B[0m"
+		echo "\x1B[33mmean:  $mean\x1B[0m"
 	fi
+	if [ $worst -le "0" ]
+	then
+		echo "\x1B[32mworst: $worst\x1B[0m"
+	else
+		echo "\x1B[33mworst: $worst\x1B[0m"
+	fi
+	echo
 
-	time
-	echo "$(time ./lem-in -t < 1 | tail -n 1)"
+	# echo "$(time ./lem-in -t < 1 | tail -n 1)"
 	rm 1
 }
 
