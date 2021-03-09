d63226a6cf5fbd2d1606f7984784972b77a6cd15
Drew Finnis <drewfinnis@gmail.com>
2021-03-09 22:54:32 +0100

    feature/test time best worst mean

--- test_performance.sh
+++ test_performance.sh
@@ -12,10 +12,29 @@ unit_test()
 	sum=0
 	best=4242
 	worst=-4242
+	sum_time=0
+	best_time=420000000000
+	worst_time=-420000000000
 	while [ $count -lt $case ]
 	do
 		target=$(./generator $MAP > 1 && cat 1 | grep "#Here" | cut -d ' ' -f 8 | head -n 1)
+		start=$(gdate +%s%N)
 		real=$(./lem-in -t < 1 | tail -n 2 | head -n 1 | cut -d ' ' -f 2)
+		end=$(gdate +%s%N)
+
+		## Time
+		runtime=$( echo "($end - $start)" | bc -l )
+		if [ $runtime -lt $best_time ]
+		then
+			best_time=$runtime
+		fi
+		if [ $runtime -gt $worst_time ]
+		then
+			worst_time=$runtime
+		fi
+		sum_time=$(($sum_time + $runtime))
+
+		## Turns
 		diff=$(($real - $target))
 		if [ $diff -lt $best ]
 		then
@@ -26,17 +45,27 @@ unit_test()
 			worst=$diff
 		fi
 		sum=$(($sum + $diff))
+
 		count=$(($count + 1))
 		echo "\r\x1B[32mPassed: $count/$count\x1B[0m\c"
 	done
 	count=$(($count - 1))
 	echo
 
-	# echo $sum
-	# echo $count
 	mean=$(($sum / $count))
+	mean_time=$(($sum_time / $count))
 
-	echo "turns taken - required:"
+	## Time decimal places
+	mean_int=$( echo "scale=0; $mean_time / 1000000000" | bc -l )
+	best_int=$( echo "scale=0; $best_time / 1000000000" | bc -l )
+	worst_int=$( echo "scale=0; $worst_time / 1000000000" | bc -l )
+
+	mean_time=$( echo "scale=3; $mean_time / 1000000000" | bc -l )
+	best_time=$( echo "scale=3; $best_time / 1000000000" | bc -l )
+	worst_time=$( echo "scale=3; $worst_time / 1000000000" | bc -l )
+
+	## turns taken - required
+	echo "Turns taken - required:"
 	if [ $best -le "0" ]
 	then
 		echo "\x1B[32mbest:  $best\x1B[0m"
@@ -55,9 +84,29 @@ unit_test()
 	else
 		echo "\x1B[33mworst: $worst\x1B[0m"
 	fi
+
+	## Time
+	echo "Time:"
+	if [ $best_int -le "15" ]
+	then
+		echo "\x1B[32mbest:  $best_time s\x1B[0m"
+	else
+		echo "\x1B[33mbest:  $best_time\x1B[0m"
+	fi
+	if [ $mean_int -le "15" ]
+	then
+		echo "\x1B[32mmean:  $mean_time s\x1B[0m"
+	else
+		echo "\x1B[33mmean:  $mean_time\x1B[0m"
+	fi
+	if [ $worst_int -le "15" ]
+	then
+		echo "\x1B[32mworst: $worst_time s\x1B[0m"
+	else
+		echo "\x1B[33mworst: $worst_time\x1B[0m"
+	fi
 	echo
 
-	# echo "$(time ./lem-in -t < 1 | tail -n 1)"
 	rm 1
 }
 
